e199e258e9fefe5f101d9985c8a04a7a981e573d
kidanemriam <kide.fikadu1@gmail.com>
2024-02-07 15:29:11 +0300

    bug fix

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facedes\Cache;
+use Illuminate\Support\Facades\Cache;
 
 class RedisController extends Controller
 {
-    function index(){
-	Cache::put('Name','Kidanemariam',60);
+	function index()
+	{
+		Cache::put('Name', 'Kidanemariam', 60);
 
-	$value=Cache::get('Name');
-	return $value;
+		$value = Cache::get('Name');
+		return $value;
+	}
 }
